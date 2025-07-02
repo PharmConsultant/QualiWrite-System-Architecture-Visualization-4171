4f514c150ff609a85dc118f4a4bdd0cf8dc2aa51
PharmConsultant <jbarnhill@altabrisa.mx>
2025-07-02 18:41:36 -0400

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -5,18 +5,17 @@ import reactRefresh from 'eslint-plugin-react-refresh';
 
 export default [
   { ignores: ['dist'] },
-  js.configs.recommended,
   {
+    extends: [js.configs.recommended],
     files: ['**/*.{js,jsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: {
         ...globals.browser,
         ...globals.node,
-        React: true,
-        JSX: true,
       },
       parserOptions: {
+        ecmaVersion: 'latest',
         ecmaFeatures: {
           jsx: true,
         },
@@ -28,12 +27,4 @@ export default [
       'react-refresh': reactRefresh,
     },
     rules: {
-      'no-undef': 'error',
-      'react-hooks/rules-of-hooks': 'error',
-      'react-hooks/exhaustive-deps': 'off',
-      'react-refresh/only-export-components': 'off',
-      'no-unused-vars': 'off',
-      'no-case-declarations': 'off',
-    },
-  },
-];
+      ...reactHooks.configs.recommended.
